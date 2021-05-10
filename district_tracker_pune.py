cb2cea59cb4766bd7e59052be2498300c4fc50ab
Aditya Sohoni <46917130+adityasohoni@users.noreply.github.com>
2021-05-10 18:08:02 +0530

    Add files via upload

---
+++ district_tracker_pune.py
@@ -0,0 +1,46 @@
+import requests
+import time
+import os
+from datetime import date
+from datetime import datetime
+district_id = '363'
+
+
+def alert(name):
+    os.system('spd-say "The vaccine is available at {}"'.format(name))
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
+
+
+while(1):
+    time.sleep(10)
+    date_var = date.today().strftime("%d-%m-%Y")
+    # date_var = '11-05-2021'
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" + district_id + "&date=" + date_var
+
+    try:
+        r = requests.get(url=URL, headers=headers)
+    except requests.ConnectionError:
+        print("No Network at " + str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+        continue
+
+    print(r.status_code)
+    data = r.json()
+
+    flag = 0
+
+    for center in data['centers']:
+        for session in center['sessions']:
+            if session['vaccine'] == 'COVISHIELD' and session['available_capacity'] > 0:
+                if center['center_id'] != 629727 and center['center_id'] != 629727:
+                    alert(center['name'])
+                    print(center['name'])
+                    print(center['address'])
+                    time.sleep(2.8)
+                    print(session)
+                    flag=1
+    if flag == 0:
+        print("Not available at " + str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
